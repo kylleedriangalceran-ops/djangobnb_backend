254c814c4ece87498f05d6e2ddbc50a82938ccfd
Kylle Edrian B. Galceran <ryan.mondega@msunaawan.edu.ph>
2025-11-24 19:06:13 +0800

    Get properties in the front end and list them, Galceran Kylle Edrian -v3

--- backend/djangobnb_backend/djangobnb_backend/urls.py
+++ backend/djangobnb_backend/djangobnb_backend/urls.py
@@ -6,5 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/properties/', include('property.urls')),
-]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
