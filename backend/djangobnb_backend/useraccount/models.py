c8099d0a94afdb27198f836af5ecbfc15e2dd9be
Kylle Edrian Galceran <kylleedrian.galceran@msunaawan.edu.ph>
2025-11-26 16:54:15 +0800

    Implement authentication Sign up, Galceran, Kylle Edrian -v4

--- backend/djangobnb_backend/useraccount/models.py
+++ backend/djangobnb_backend/useraccount/models.py
@@ -1,7 +1,7 @@
 import uuid
 
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser,PermissionsMixin, UserManager
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin, UserManager
 from django.db import models
 
 
@@ -13,7 +13,7 @@ class CustomUserManager(UserManager):
         email = self.normalize_email(email)
         user = self.model(name=name, email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save(using=self.db)
 
         return user
     
@@ -22,13 +22,14 @@ class CustomUserManager(UserManager):
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(name, email, password, **extra_fields)
     
+
     def create_superuser(self, name=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self._create_user(name, email, password, **extra_fields)
     
 
-class User(AbstractUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255, blank=True, null=True)
